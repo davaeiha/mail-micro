53e330a5ba5138d3b97b4639407af487ef43c64a
mahdi.davaeiha <m.m.davaeiha@gmail.com>
2022-05-14 10:22:18 +0430

    basic smtp module and smtp configuration

---
+++ prisma/migrations/20220509144437_mail/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `client` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(80) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME NOT NULL,
+
+    UNIQUE INDEX `client_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `mail` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `from_email` VARCHAR(80) NOT NULL,
+    `subject` VARCHAR(255) NULL,
+    `body` LONGTEXT NULL,
+    `text` TEXT NULL,
+    `send_at` DATETIME NULL,
+    `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `receiver` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `mail_id` INTEGER NULL,
+    `to_email` VARCHAR(80) NOT NULL,
+    `type` ENUM('1', '2', '3') NOT NULL DEFAULT '1',
+    `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `receiver` ADD CONSTRAINT `receiver_mail_id_fkey` FOREIGN KEY (`mail_id`) REFERENCES `mail`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/smtp/smtp.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { SmtpService } from './smtp.service';
+import { PrismaService } from '../prisma.service';
+import { SmtpConsumerService } from './smtp.consumer.service';
+import { MailConsumerService } from '../kafka/mail.consumer.service';
+import { MailProducerService } from '../kafka/mail.producer.service';
+
+@Module({
+  providers: [
+    SmtpService,
+    PrismaService,
+    SmtpConsumerService,
+    MailConsumerService,
+    MailProducerService,
+  ],
+  controllers: [],
+})
+export class SmtpModule {}
